8f43adef2765e702c9262a00e038205282444097
Becem Zaghden <zaghdenb@gmail.com>
2019-08-19 10:01:54 +0100

    app completed

--- js/index.js
+++ js/index.js
@@ -4,63 +4,51 @@
   var minutes = "00";
   var secondsDiv = document.getElementById("seconds");
   var minutesDiv = document.getElementById("minutes");
+  // this function will execute after the first card reveal (line 157).
 
-  // this function execute after the first card reveal
-  /*
-  timer = () => {
-    secondsDiv.innerText = seconds;
-    minutesDiv.innerText = minutes;
-    setTimeout(() => {
-      if (seconds < 60) {
-        seconds++;
-        if (seconds < 10) {
-          // just so i have the 00:00 format
-          seconds = "0" + seconds;
-        }
-        timer();
-      } else {
-        minutes++;
-        seconds = "00";
-        if (minutes < 10) {
-          minutes = "0" + minutes;
-        }
-        timer();
-      }
-    }, 1000);
-  }
-*/
-  function timer(){
-    setInterval( () => {
-
-    secondsDiv.innerText = seconds;
-    minutesDiv.innerText = minutes;
-    
-      if (seconds <= 60) {
-        seconds++;
-        if (seconds < 10) {
-          // just so i have the dd:dd format
-          seconds = "0" + seconds;
-        }
-      }
-      else {
-        minutes++;
-        seconds = "00";
-        if (minutes < 10) {
-          minutes = "0" + minutes;
-        }
+  function timer() {
+    seconds++;
+    if (seconds < 10) {
+      // just so i have the dd:dd format
+      seconds = "0" + seconds;
+    }
+    if (seconds >= 60) {
+      minutes++;
+      seconds = "00";
+      if (minutes < 10) {
+        minutes = "0" + minutes;
       }
     }
-    ,1000)
-    
-  }
-  
+    secondsDiv.innerText = seconds;
+    minutesDiv.innerText = minutes;
 
-  var items = ["spa", "ac_unit", "gesture", "control_camera", "polymer", "gavel", "local_florist", "whatshot",
-   "spa", "ac_unit", "gesture", "control_camera", "polymer", "gavel", "local_florist", "whatshot"]
-  /****** shuffling program ******/
-  //const SYMBOLS = [1, 2, 3, 4, 5, 6, 7, 8, 1, 2, 3, 4, 5, 6, 7, 8];
-  const SYMBOLS = ["spa", "ac_unit", "gesture", "control_camera", "polymer", "gavel", "local_florist", "whatshot",
-  "spa", "ac_unit", "gesture", "control_camera", "polymer", "gavel", "local_florist", "whatshot"]
+    if (moves == 0) {
+      runTimer = setInterval(timer, 1000);
+    }
+  }
+
+
+
+  /*       shuffling program       */
+  // icons names from material design.
+  const SYMBOLS = [
+    "spa",
+    "ac_unit",
+    "gesture",
+    "control_camera",
+    "polymer",
+    "gavel",
+    "local_florist",
+    "whatshot",
+    "spa",
+    "ac_unit",
+    "gesture",
+    "control_camera",
+    "polymer",
+    "gavel",
+    "local_florist",
+    "whatshot"
+  ];
   var cardsDiv = document.getElementById("cards");
   var randomSymbols = [];
 
@@ -75,124 +63,129 @@
     for (let i = 0; i < 16; i++) {
       var card = document.createElement("div");
       //card.innerHTML = `<i class="fas fa-${items[i]}"></i>`;
-      card.innerHTML = `<i class="material-icons orange600">${randomSymbols[i]}</i>`;
+      card.innerHTML = `<i class="material-icons some md-36 orange600">${
+        randomSymbols[i]
+      }</i>`;
       card.classList.add(randomSymbols[i], "card");
       card.addEventListener("click", cardClicked);
       cardsDiv.appendChild(card);
     }
   })();
 
-  /****** clicked cards handler ******/
 
-  // this program (cardClicked) take's care of
+
+  /*      clicked cards handler      */
+
+  // this program (cardClicked) take's care of:
   // 1- reveal the hidden symbol behind the clicked card (with fucntion revealCard(e)).
-  // 2- acknowledge the card clicked and execute the game logic ( function cardsChecker(e)).
-  // 3- placeholder
-/*
-  function cardClicked(e) {
-    cardsChecker(e)
-    revealCard(e)
-  }
-  */
-  var movesDiv = document.getElementById('moves');
-  
+  // 2- acknowledge the card clicked and execute the game logic ( function cardsHandler(e)).
+  // 3- keep track of moves and stars rating (function ScoreHandler).
+
+  var movesDiv = document.getElementById("moves");
   var moves = 0;
   movesDiv.innerText = "Moves: " + moves;
 
-  function revealCard(e) {   
+  function revealCard(e) {
     e.target.classList.add("clicked");
     e.target.removeEventListener("click", cardClicked);
-    /*
-    if (seconds === "00" && minutes === "00") {
-      timer();
-    }*/
   }
 
   var firstCard, secondCard, firstCardDiv;
-  var score = 1;
+  var score = 3;
+  var matchedCards = 0;
   var scoreDiv = document.querySelector("#score-board");
 
-  
   function cardClicked(e) {
-    
     if (firstCard === undefined) {
       firstCard = e.target.classList[0];
       firstCardDiv = e.target;
-      console.log(`this is firstCard = ${firstCard}`);revealCard(e)
+      console.log(`this is firstCard = ${firstCard}`);
+      revealCard(e);
     } else if (firstCard !== undefined && secondCard === undefined) {
       secondCard = e.target.classList[0];
-      console.log(`this is SecondCard = ${secondCard}`);revealCard(e)
+      console.log(`this is SecondCard = ${secondCard}`);
+      revealCard(e);
       setTimeout(() => {
-      // this is the next step :p execute the game logic and restart data to initial state
-      if (firstCard === secondCard) {
-        e.target.classList.replace("clicked", "correct");
-        firstCardDiv.classList.replace("clicked", "correct");
-        scoreChecker();
-      } else {
+        // this is the next step :p execute the game logic and restart data to initial state
+        if (firstCard === secondCard) {
+          e.target.classList.replace("clicked", "correct");
+          firstCardDiv.classList.replace("clicked", "correct");
+          matchedCards++;
+          scoreHandler("match");
+          Wining();
+        } else {
           e.target.classList.remove("clicked");
           firstCardDiv.classList.remove("clicked");
           e.target.addEventListener("click", cardClicked);
           firstCardDiv.addEventListener("click", cardClicked);
-      }
-    
-      if (moves === 1) {
-        timer();
-      }
-      
-      firstCard = undefined;
-      secondCard = undefined;
-      moves++;
-      movesDiv.innerText = "Moves: " + moves;
-    }, 700);
+          scoreHandler("noMatch");
+        }
+
+        firstCard = undefined;
+        secondCard = undefined;
+        moves++;
+        movesDiv.innerText = "Moves: " + moves;
+      }, 700);
     }
-    
   }
-    /******* Score Handler ******/
-    function scoreChecker() {
+  var stars = "";
+  var star = '<i class="material-icons">star</i>';
+  for (let i = 1; i <= score; i++) {
+    stars += star;
+  }
+  scoreDiv.innerHTML = stars;
+
+
+
+  /*      Score Handler       */
+  function scoreHandler(arg) {
+    if (arg === "match") {
       score++;
-      scoreDiv.innerText = score;
-      console.log(score);
-      if (score === 9) {
-        setTimeout(function alertWin() {
-          alert("You Win!!!!");
-        }, 1000);
+      stars += star;
+    } else if (arg === "noMatch") {
+      if (score == 1) {
+        stars = star;
+      } else {
+        score--;
+        stars = "";
+        for (let i = 1; i <= score; i++) {
+          stars += star;
+        }
       }
     }
+    scoreDiv.innerHTML = stars;
 
-    console.log(firstCard);
-    console.log(secondCard);
-  
+    // start timer after the first move
+    timer();
 
-  /***** moves handler *****/
+  }
 
-  
-  /***** restart button *****/
-  // the restart button restart score, timer, moves-counter to initial state and shuffle the cards
-  var restartButton = document.querySelector("#restart-button");
-  restartButton.addEventListener("click", () => {
-    console.log(
-      `moves: ${moves} your timing is ${parseInt(minutes, 10)} minutes and ${parseInt(
-        seconds,
-        10
-      )} seconds`
+
+
+  /*        Congratulations Popup        */
+  function Wining(){
+  if (matchedCards == 8) {
+    var winningWindow = document.createElement('div');
+    document.body.appendChild(winningWindow);
+    winningWindow.classList.add("winning-window");
+    winningWindow.innerHTML = `
+    <h3>Congratulations!!</h3>
+    <p>You have completed in ${minutes} minutes and ${seconds} seconds.</p>
+    <div>${stars}</div>
+    <button onclick="restart()">Play again</button>
+    `
+    setTimeout(() => {
+    winningWindow.classList.add("animated")}
+    , 100
     );
-    console.log(`your score ${score}`);
-    seconds = "00";
-    minutes = "00";
-    score = 1;
-    scoreDiv.innerText = score;
-    moves = 0;
-    movesDiv.innerText = "Moves: " + moves;
-    const CARDS = document.querySelectorAll(".card");
-    for (let card of CARDS) {
-      card.parentNode.removeChild(card);
-    }
+    clearInterval(runTimer);
+  }
+  }
 
-    firstCard = undefined;
-    randomSymbols = [];
-    shuffle();
+  /*       restart button      */
+  // the restart button reset the game board, the timer, and the star rating.
+  var restartButton = document.querySelector("#restart-button");
+  restartButton.addEventListener("click", restart = () => {
+    location.reload();
   });
-  
-
-
 })();
